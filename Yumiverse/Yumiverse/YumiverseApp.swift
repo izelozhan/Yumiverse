eb950f6961bdbb3c3fde110991d9106c88e1f0fe
Izel Ozhan <izelozhan@gmail.com>
2025-08-18 12:49:16 -0500

    init

---
+++ Yumiverse/Yumiverse/YumiverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumiverseApp.swift
+//  Yumiverse
+//
+//  Created by Izel on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct YumiverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
